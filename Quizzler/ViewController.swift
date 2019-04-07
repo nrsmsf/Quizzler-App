2220559dfbbb9337b21723b4aa0b0a5c626e03d5
nrsmsf <nrsmsf@ya.ru>
2019-04-07 13:01:18 +0300

    adding restart mechanism

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer:Bool = false
+    var questionNumber:Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,11 +22,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text =  firstQuestion.questionText
+        
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1{
+            pickedAnswer = true
+            
+        }
+        else if sender.tag == 2{
+            pickedAnswer = false
+        }
+        checkAnswer()
+        questionNumber = questionNumber + 1
+        nextQuestion()
     }
     
     
@@ -35,19 +48,41 @@ class ViewController: UIViewController {
     
 
     func nextQuestion() {
+    
         
+        if questionNumber <= 12{
+            questionLabel.text = allQuestions.list[questionNumber].questionText
+            
+        }
+            
+        else{
+            let alert = UIAlertController(title: "Awesome1", message: "You have passed all the questions. Do you want to restart?", preferredStyle: .alert)
+            let restartAction = UIAlertAction(title: "Restart", style: .default) { (UIAlertAction) in
+                self.startOver()
+                self.nextQuestion()
+            }
+            alert.addAction(restartAction)
+            present(alert, animated: true, completion: nil)
+        }
     }
     
     
     func checkAnswer() {
-        
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        if correctAnswer == pickedAnswer {
+            print("You are good guy!")
+        }
+        else{
+            print("Try one more time")
+        }
+      
+
     }
     
     
     func startOver() {
-       
+       questionNumber = 0
     }
-    
-
+  
     
 }
